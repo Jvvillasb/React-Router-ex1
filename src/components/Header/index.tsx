a10611ec16bdb1e9fc9f48a30e962dc548270aba
João Vitor Villas Bôas <jvvillasb@gmail.com>
2024-01-15 22:12:02 -0300

    Layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
 export default function Header() {
-    return(
+
+    return (
         <header>
-            <div>
-                <h1>Meu Site</h1>
+            <div className="container">
+                <Link to="/">
+                    <h1 className="site-name">MeuSite</h1>                
+                </Link>
             </div>
         </header>
-    )
+    );
 }
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/Header";
 
 export default function Home() {
+
     return (
         <>
-            <Header/>
-            <Outlet/>
+            <Header />
+            <Outlet />
         </>
     )
 }
